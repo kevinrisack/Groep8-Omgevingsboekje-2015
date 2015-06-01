737d4ab0f924954b3723ad161bdab09b75cf033f
Niels Deryckere <niels.deryckere@student.howest.be>
2015-06-01 15:01:38 +0200

    Added entity framework

---
+++ DigitaalOmgevingsboek/Models/Thema.cs
@@ -0,0 +1,27 @@
+namespace DigitaalOmgevingsboek
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Thema")]
+    public partial class Thema
+    {
+        public Thema()
+        {
+            POI = new HashSet<POI>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ThemaNaam { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public virtual ICollection<POI> POI { get; set; }
+    }
+}
