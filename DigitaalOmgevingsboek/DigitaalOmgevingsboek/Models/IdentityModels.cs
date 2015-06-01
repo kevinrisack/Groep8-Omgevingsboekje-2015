a0e7bbfc6756c4236d4b122fe698b0ac46271e4c
Niels Deryckere <niels.deryckere@student.howest.be>
2015-06-01 09:36:32 +0200

    Added backup database + migration asp-identities

--- DigitaalOmgevingsboek/DigitaalOmgevingsboek/Models/IdentityModels.cs
+++ DigitaalOmgevingsboek/DigitaalOmgevingsboek/Models/IdentityModels.cs
@@ -11,11 +11,15 @@ namespace DigitaalOmgevingsboek.Models
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
+           
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
             // Add custom user claims here
             return userIdentity;
         }
+        public string Name{get;set;}
+        public string Firstname { get; set; }
+      
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
