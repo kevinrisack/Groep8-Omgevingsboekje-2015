25a0cee2797546c3aaf9d8a548cd5ea7f2cead40
Niels Deryckere <niels.deryckere@student.howest.be>
2015-06-01 11:37:05 +0200

    Models part niels+changes database

---
+++ DigitaalOmgevingsboek/DigitaalOmgevingsboek/Models/Link.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DigitaalOmgevingsboek.Models
+{
+    public class Link
+    {
+        public int Id { get; set; }
+
+        public string URL { get; set; }
+        public int Activiteit_Id { get; set; }
+    }
+}
---
+++ DigitaalOmgevingsboek/DigitaalOmgevingsboek/Models/POI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DigitaalOmgevingsboek.Models
+{
+    public class POI
+    {
+
+       
+        public int Id { get; set; }
+
+
+         [Required]
+         [StringLength(50, MinimumLength = 6, ErrorMessage = "De naam moet minimum 6 characters bevatten")]
+        public string Naam { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Het adres moet minimum 6 characters bevatten")]
+        [Display(Name="Straat + nummer")]
+         public string Adres { get; set; }
+
+        [Required]
+        public string Gemeente { get; set; }
+
+        public string Telefoon { get; set; }
+
+        [EmailAddress(ErrorMessage = "U heeft een onjuist emailadres ingegeven")]
+        public string Email { get; set; }
+
+        [Display(Name="Website locatie")]
+        public string WebsiteURL { get; set; }
+
+        public string Openingsuur { get; set; }
+
+        [DataType(DataType.Currency,ErrorMessage="Geef een geldige waarde op. vb: 2,08")]
+        public float Toegangsprijs { get; set; }
+
+        [MinLength(6,ErrorMessage="De beschrijving moet minimum 6 karakters bevatten")]
+        public string Beschrijving { get; set; }
+
+        public string Contactpersoon_Naam { get; set; }
+
+        [EmailAddress(ErrorMessage = "U heeft een onjuist emailadres ingegeven")]
+        public string Contactpersoon_Email { get; set; }
+
+        public string Auteur_Id { get; set; }
+
+        public DateTime TimeCreated { get; set; }
+        public DateTime TimeModified { get; set; }
+        public string Duurtijd { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+      
+                
+
+    }
+}
