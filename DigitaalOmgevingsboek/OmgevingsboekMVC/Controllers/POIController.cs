605340067435b72a87d8bed27b3d210a8b3ba823
Risack Kevin <Kevin@MacBook-Pro-van-Kevin.local>
2015-06-02 15:04:27 +0200

    Wep API project --> MVC Project

---
+++ DigitaalOmgevingsboek/OmgevingsboekMVC/Controllers/POIController.cs
@@ -0,0 +1,49 @@
+﻿using OmgevingsboekMVC.Businesslayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigitaalOmgevingsboek.Controllers
+{
+    public class POIController : Controller
+    {
+        private IPOIService ps;
+
+        public POIController(IPOIService ps)
+        {
+            this.ps = ps;
+        }
+
+        // GET: POI
+        public ActionResult POIStart()
+        {
+            return View();
+        }
+
+        public ActionResult POIOverzicht()
+        {
+            List<POI> pois = ps.GetPOIs();
+            return View(pois);
+        }
+
+        public ActionResult POINewModify()
+        {
+            return View();
+        }
+
+
+        public ActionResult POIActivity()
+        {
+            return View();
+        }
+
+
+        public ActionResult POIView(int id)
+        {
+            
+            return View();
+        }
+    }
+}
