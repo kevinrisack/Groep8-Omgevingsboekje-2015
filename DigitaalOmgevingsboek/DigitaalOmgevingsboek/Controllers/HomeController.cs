9e80e40e6de6d04e17ef8060c0631a184929029b
Risack Kevin <Kevin@MacBook-Pro-van-Kevin.local>
2015-06-02 10:40:27 +0200

    Revert "Eerste deelt Layout toegevoegd"
    This reverts commit de79d3967ed7daa57c5244a4edcc1a451575196d, reversing
    changes made to 9be6c6d29c421ee6fa1cae06701e63ee8eaabc43.

--- DigitaalOmgevingsboek/DigitaalOmgevingsboek/Controllers/HomeController.cs
+++ DigitaalOmgevingsboek/DigitaalOmgevingsboek/Controllers/HomeController.cs
@@ -4,25 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace BootstrapSite1.Controllers
+namespace DigitaalOmgevingsboek.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Title="Testcase";
-return View();
-        }
+            ViewBag.Title = "Home Page";
 
-        public ActionResult Home()
-        {
-            ViewBag.Title = "Testcase";
-            return View();
-        }
-
-        public ActionResult Uitstappen()
-        {
-            ViewBag.Title = "Testcase";
             return View();
         }
     }
--- DigitaalOmgevingsboek/DigitaalOmgevingsboek/Controllers/POIController.cs
+++ DigitaalOmgevingsboek/DigitaalOmgevingsboek/Controllers/POIController.cs
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 ﻿using DigitaalOmgevingsboek.Businesslayer.Services;
 using System;
-=======
-﻿using System;
->>>>>>> Layout
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-<<<<<<< HEAD
 namespace DigitaalOmgevingsboek.Controllers
 {
     public class POIController : Controller
@@ -24,38 +19,6 @@ namespace DigitaalOmgevingsboek.Controllers
         {
             List<POI> pois = ps.GetPOIs();
             return View(pois);
-=======
-namespace BootstrapSite1.Controllers
-{
-    public class POIController : Controller
-    {
-        // GET: POI
-        public ActionResult POIStart()
-        {
-            return View();
-        }
-
-        public ActionResult POIOverzicht()
-        {
-            return View();
-        }
-
-        public ActionResult POINewModify()
-        {
-            return View();
-        }
-
-
-        public ActionResult POIActivity()
-        {
-            return View();
-        }
-
-
-        public ActionResult POIView()
-        {
-            return View();
->>>>>>> Layout
         }
     }
 }
