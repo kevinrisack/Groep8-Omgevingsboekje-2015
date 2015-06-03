1e4e7a47fd280a9025f18c402b98c4fbb5372541
Risack Kevin <kevin.risack@student.howest.be>
2015-06-03 14:14:10 +0200

    Uitstappen Overzicht

---
+++ DigitaalOmgevingsboek/OmgevingsboekMVC/Controllers/UitstapController.cs
@@ -0,0 +1,27 @@
+﻿using DigitaalOmgevingsboek;
+using OmgevingsboekMVC.Businesslayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OmgevingsboekMVC.Controllers
+{
+    public class UitstapController : Controller
+    {
+        UitstapService us;
+
+        public UitstapController(UitstapService us)
+        {
+            this.us = us;
+        }
+        
+        // GET: Uitstap
+        public ActionResult Index()
+        {
+            IEnumerable<Uitstap> uitstappen = us.GetUitappen();
+            return View(uitstappen);
+        }
+    }
+}
