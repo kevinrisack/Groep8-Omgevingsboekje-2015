8528664aef21bc492ac5bd60b20ccf23c7e43250
Risack Kevin <kevin.risack@student.howest.be>
2015-06-03 15:09:02 +0200

    Uitstappen Basic

--- DigitaalOmgevingsboek/OmgevingsboekMVC/Controllers/UitstapController.cs
+++ DigitaalOmgevingsboek/OmgevingsboekMVC/Controllers/UitstapController.cs
@@ -23,5 +23,29 @@ namespace OmgevingsboekMVC.Controllers
             IEnumerable<Uitstap> uitstappen = us.GetUitappen();
             return View(uitstappen);
         }
+
+        public ActionResult Edit()
+        {
+            Uitstap uitstap = new Uitstap();
+            return View(uitstap);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Uitstap uitstap = us.GetUitstap(id);
+            return View(uitstap);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Uitstap uitstap = us.GetUitstap(id);
+            return View(uitstap);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Uitstap uitstap = us.GetUitstap(id);
+            return View(uitstap);
+        }
     }
 }
