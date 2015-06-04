bad55f71fed612e9b67b54a56aa7afaca5a54a62
Niels Deryckere <niels.deryckere@student.howest.be>
2015-06-04 11:18:14 +0200

    Accountvieuws restored

--- DigitaalOmgevingsboek/DigitaalOmgevingsboek/Businesslayer/Services/IPOIService.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace DigitaalOmgevingsboek.Businesslayer.Services
-{
-    public interface IPOIService
-    {
-        System.Collections.Generic.List<DigitaalOmgevingsboek.POI> GetPOIs();
-    }
-}
